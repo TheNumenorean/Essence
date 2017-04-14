22d8d77d9c3d1eff173065eadc0db0459ccc50a7
Francesco Macagno <fjmacagno1@yahoo.com>
2017-04-13 18:44:08 -0700

    Added FFmpeg java interface

---
+++ runtime/src/main/java/net/thenumenorean/essence/media/InputFile.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package net.thenumenorean.essence.media;
+
+import java.net.URL;
+
+/**
+ * @author Francesco
+ *
+ */
+public class InputFile {
+
+	/**
+	 * 
+	 */
+	public InputFile(URL url) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
