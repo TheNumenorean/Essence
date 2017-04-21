653fe93e4ec4cd34e51a1fef57d36a95c5c3cc25
Francesco Macagno <fjmacagno1@yahoo.com>
2017-04-20 22:56:06 -0700

    small changes

--- runtime/src/main/java/net/thenumenorean/essence/EssenceRuntime.java
+++ runtime/src/main/java/net/thenumenorean/essence/EssenceRuntime.java
@@ -5,6 +5,7 @@ package net.thenumenorean.essence;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 /**
@@ -17,9 +18,16 @@ public class EssenceRuntime implements Runnable {
 	static final String EZSTREAM_LOG = "log/exstream.log";
 
 	static final File TRACK_DIR = new File("tracks/");
+	static final File UPLOADS_DIR = new File(TRACK_DIR, "uploads/");
 	static final File OUT_FILE = new File(TRACK_DIR, "current/next.mp3");
 
 	static final String testvid = "https://www.youtube.com/watch?v=uE-1RPDqJAY&t=3s";
+	
+	
+	
+	private ProcessBuilder pb;
+	private Thread upNextPlacer;
+	Process ezstream;
 
 	/**
 	 * 
@@ -27,19 +35,21 @@ public class EssenceRuntime implements Runnable {
 	public EssenceRuntime() {
 
 		TRACK_DIR.mkdir();
+		
+		pb = new ProcessBuilder("ezstream -c " + STREAM_CONF);
+		pb.redirectErrorStream(true);
+		pb.redirectOutput(new File(EZSTREAM_LOG));
+		
+
+		upNextPlacer = new Thread(new UpNextPlacer(this));
 	}
 
 	@Override
 	public void run() {
 		
-		ProcessBuilder pb = new ProcessBuilder("ezstream -c " + STREAM_CONF);
-		pb.redirectErrorStream(true);
-		pb.redirectOutput(new File(EZSTREAM_LOG));
-		
-		Thread upNextPlacer = new Thread(new UpNextPlacer(this));
 		
 		try {
-			Process ezstream = pb.start();
+			ezstream = pb.start();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
@@ -50,6 +60,10 @@ public class EssenceRuntime implements Runnable {
 	}
 
 	private void findNewTracks() {
+		
+		File[] uploads = UPLOADS_DIR.listFiles(new NoFolderFilter());
+		
+		
 
 	}
 
@@ -62,19 +76,19 @@ public class EssenceRuntime implements Runnable {
 	public File getNextTrack() {
 		// TODO make not random
 		
-		File[] tracks = TRACK_DIR.listFiles(new FileFilter(){
-
-			@Override
-			public boolean accept(File pathname) {
-				return pathname.isFile();
-			}
-			
-		});
+		File[] tracks = TRACK_DIR.listFiles(new NoFolderFilter());
 		
 		return tracks.length < 1 ? null : tracks[0];
 		
 	}
+	
+	private class NoFolderFilter implements FileFilter {
 
-
+		@Override
+		public boolean accept(File pathname) {
+			return pathname.isFile();
+		}
+		
+	}
 
 }
