e70930af4e328ec7f7fa5db82bfd8a09e5a9859b
Francesco Macagno <fjmacagno1@yahoo.com>
2017-05-17 20:04:39 -0700

    Redid track processing to work better with videos with unpredictable outputs

--- runtime/src/main/java/net/thenumenorean/essence/media/TrackProcessor.java
+++ runtime/src/main/java/net/thenumenorean/essence/media/TrackProcessor.java
@@ -6,6 +6,7 @@ package net.thenumenorean.essence.media;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -54,97 +55,159 @@ public class TrackProcessor extends RepeatingRunnable {
 	 */
 	@Override
 	public void loop() {
+
+		// Get unprocessed tracks
 		for (Document doc : trackSource.find(Filters.eq("processed", false))) {
 			String currLoc = doc.getString("location");
-			File out = null;
-			boolean isWeb = false;
-			if (currLoc == null || currLoc.isEmpty()) {
-				isWeb = true;
-				out = downloadVideo(doc.getString("webaddress"));
-				if(out == null) {
-					EssenceRuntime.log.severe("Couldnt get file from web!");
-					continue;
-				}
-			} else {
-				out = new File(currLoc);
-			}
-			
-			
-				
 
-			File to = new File(EssenceRuntime.TRACK_DIR,
-					out.getName().substring(0, out.getName().indexOf('.')) + ".mp3");
-			audioEncoder.convert(out, to);
-			out.delete();
-			if (isWeb) {
-				deleteFolder(out.getParentFile());
+			EssenceRuntime.log.info("Processing video " + doc.getString("name"));
+
+			File result = null;
+			if (currLoc == null || currLoc.isEmpty()) { // Is a web video
+
+				// Attempt to download the video
+				result = getInternetTrack(doc.getString("webaddress"));
+				if (result == null)
+					EssenceRuntime.log.severe("Couldnt get file from web!");
+
+			} else { // Uploaded file
+
+				File upload = new File(currLoc);
+				try {
+
+					result = new File(EssenceRuntime.TRACK_DIR,
+							upload.getName().substring(0, upload.getName().indexOf('.')) + ".mp3");
+					audioEncoder.convert(upload, result);
+
+				} catch (Exception e) {
+					EssenceRuntime.log
+							.warning("Error occured converting track " + upload.getPath() + ": " + e.getMessage());
+					result = null;
+				} finally {
+					upload.delete(); // Delete the uploaded file regardless
+				}
+
 			}
-			
-			trackSource.replaceOne(Filters.eq("_id", doc.getObjectId("_id")), doc.append("location", to.getPath()).append("processed", true));
+
+			if (result != null) {
+				trackSource.replaceOne(Filters.eq("_id", doc.getObjectId("_id")),
+						doc.append("location", result.getPath()).append("processed", true));
+			} else {
+				trackSource.deleteOne(Filters.eq("_id", doc.getObjectId("_id")));
+			}
 
 		}
 
 	}
 
 	/**
-	 * Downloads the video from the given web link and then chooses the best candidate file for extracting the soundtrack and returns it.
-	 * @param url The url in string format to download the video from
+	 * Downloads the video from the given web link and then chooses the best
+	 * candidate file for extracting the soundtrack. It then converts the format
+	 * to mp3 and copies it to the tracks folder.
+	 * 
+	 * @param url
+	 *            The url in string format to download the video from
 	 * @return A file, or null if an error occured
 	 */
-	private File downloadVideo(String url) {
+	private File getInternetTrack(String url) {
 
-		EssenceRuntime.log.info("Downloading video at " + url);
+		// Create a temporary folder to download files into
+		File tmp = new File(DEFAULT_DOWNLOADS_DIR + (new Random().nextInt()));
+		tmp.mkdirs();
 
+		// Download all available files
+		File[] files;
 		try {
-			// Make sure the destination exists
-			File tmp = new File(DEFAULT_DOWNLOADS_DIR + (new Random().nextInt()));
-			tmp.mkdirs();
-
-			// Get the actual files
-			VGet get = new VGet(new URL(url), tmp);
-			get.download();
-
-			// filter the results to only include valid candidate files
-			File[] res = tmp.listFiles(new FileFilter() {
-
-				@Override
-				public boolean accept(File pathname) {
-					if (pathname.isDirectory())
-						return false;
-					return true;
-				}
-
-			});
-
-			if (res.length < 1) {
-				EssenceRuntime.log.severe("Video download didnt result in any usable files");
-				return null;
-			}
-
-			Arrays.sort(res, 0, res.length, new Comparator<File>() {
-
-				@Override
-				public int compare(File o1, File o2) {
-					
-					// Sort by type so we take the most preferable
-					if(o1.getName().endsWith(".webm") && !o2.getName().endsWith(".webm")) {
-						return -1;
-					}
-					
-					return o1.getName().compareTo(o2.getName());
-				}
-
-			});
-
-			EssenceRuntime.log.info("Download finished, result file: " + res[0].getName());
-
-			return res[0];
-
+			files = downloadVideo(url, tmp);
 		} catch (Exception e) {
 			EssenceRuntime.log.severe("Error downloading video at " + url + ": " + e.getMessage());
 			e.printStackTrace();
 			return null;
 		}
+
+		// If no files, report failiure
+		if (files == null || files.length < 1) {
+			EssenceRuntime.log.severe("Video download didnt result in any usable files");
+			return null;
+		}
+
+		// Sort the files by which ones are most preferable
+		Arrays.sort(files, 0, files.length, new Comparator<File>() {
+
+			@Override
+			public int compare(File o1, File o2) {
+
+				// Sort by type so we take the most preferable
+				if (o1.getName().endsWith(".webm") && !o2.getName().endsWith(".webm")) {
+					return -1;
+				}
+
+				return o1.getName().compareTo(o2.getName());
+			}
+
+		});
+
+		File newTrack = null;
+
+		// Go through each file and attempt to generate a valid output file
+		for (File f : files) {
+			newTrack = new File(EssenceRuntime.TRACK_DIR, f.getName().substring(0, f.getName().indexOf('.')) + ".mp3");
+
+			try {
+				audioEncoder.convert(f, newTrack);
+
+				break; // If the conversion succedded, this is a good file
+
+			} catch (Exception e) {
+				// If an error occurs, continue so we can try to see if other
+				// files may work
+				EssenceRuntime.log.warning("Error occured converting track " + f.getPath() + ": " + e.getMessage());
+				newTrack = null; // Set to null so if no files work we can tell
+			}
+		}
+
+		// Check if there was asuccess
+		if (newTrack == null)
+			EssenceRuntime.log.warning("All files failed to convert!");
+
+		// Delete the tmp directory, regardless of success
+		deleteFolder(tmp);
+
+		return newTrack;
+
+	}
+
+	/**
+	 * Download all available files from the given address.
+	 * 
+	 * @param url
+	 *            The address to download from
+	 * @param downloadDir
+	 *            The drectory to download to
+	 * @return An array of Files. Each object points to an actual file, and not
+	 *         a folder. If no files were found, the array will be empty. If an
+	 *         error occured, the return may be null.
+	 * @throws MalformedURLException
+	 */
+	private File[] downloadVideo(String url, File downloadDir) throws MalformedURLException {
+
+		// Get the actual files
+		VGet get = new VGet(new URL(url), downloadDir);
+		get.download();
+
+		// filter the results to only include valid candidate files
+		File[] res = downloadDir.listFiles(new FileFilter() {
+
+			@Override
+			public boolean accept(File pathname) {
+				if (pathname.isDirectory())
+					return false;
+				return true;
+			}
+
+		});
+
+		return res;
 
 	}
 
