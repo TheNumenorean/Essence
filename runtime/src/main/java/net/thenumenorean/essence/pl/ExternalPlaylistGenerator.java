7307e3788ae7bc66ff479596bd5de74b3bf2671d
Francesco Macagno <fjmacagno1@yahoo.com>
2017-06-07 23:18:03 -0700

    Finished implementation of externelplaylistgenerator

--- runtime/src/main/java/net/thenumenorean/essence/pl/ExternalPlaylistGenerator.java
+++ runtime/src/main/java/net/thenumenorean/essence/pl/ExternalPlaylistGenerator.java
@@ -5,6 +5,8 @@ package net.thenumenorean.essence.pl;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 
 import org.bson.Document;
@@ -12,37 +14,64 @@ import org.bson.Document;
 import net.thenumenorean.essence.MongoDriver;
 
 /**
+ * This playlist generator uses an external script or program to calculate what
+ * the new playlist should be. Data is passed using json and stdin and stdout.
+ * 
  * @author Francesco
  *
  */
 public class ExternalPlaylistGenerator extends PlaylistGenerator {
-	
+
 	private static File MODULE_DIR = new File("modules");
 	private static String MODULE_START_SCRIPT_NAME = "run.sh";
-	
+
 	private File modRunScript;
-	
 
 	/**
+	 * Create a new generator with the given name of the module to run
+	 * 
 	 * @param md
-	 * @param name 
+	 *            MongoDriver to get data from
+	 * @param name
+	 *            The name of the module to run. It will be looked for in
+	 *            modules/"name"/run.sh
 	 */
 	public ExternalPlaylistGenerator(MongoDriver md, String name) throws FileNotFoundException {
 		super(md);
-		
+
 		modRunScript = new File(MODULE_DIR, name + File.pathSeparatorChar + MODULE_START_SCRIPT_NAME);
-		
-		if(!modRunScript.exists() || modRunScript.isDirectory())
+
+		if (!modRunScript.exists() || modRunScript.isDirectory())
 			throw new FileNotFoundException("Couldnt find module at " + modRunScript.getPath());
-		
+
 	}
 
-	/* (non-Javadoc)
-	 * @see net.thenumenorean.essence.pl.PlaylistGenerator#generatePlaylist(java.util.List, java.util.List)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.thenumenorean.essence.pl.PlaylistGenerator#generatePlaylist(java.util
+	 * .List, java.util.List)
 	 */
 	@Override
 	public List<Document> generatePlaylist(List<Document> currentPlaylist, List<Document> requests) {
-		// TODO Auto-generated method stub
+
+		ProcessBuilder pb = new ProcessBuilder("sh", modRunScript.getPath());
+		pb.redirectErrorStream(false);
+		pb.redirectInput(Redirect.PIPE);
+
+		Process p;
+		try {
+			p = pb.start();
+
+			PlaylistGenerator.writeDocumentsToStream(requests, p.getOutputStream());
+
+			return PlaylistGenerator.readDocumentsFromStream(p.getInputStream());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 
--- runtime/src/main/java/net/thenumenorean/essence/pl/PlaylistGenerator.java
+++ runtime/src/main/java/net/thenumenorean/essence/pl/PlaylistGenerator.java
@@ -3,6 +3,13 @@
  */
 package net.thenumenorean.essence.pl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.Document;
@@ -14,6 +21,9 @@ import net.thenumenorean.essence.MongoDriver;
  *
  */
 public abstract class PlaylistGenerator {
+	
+
+	public static String DOC_HEADER = "RequestDocumentJSON:";
 
 	protected MongoDriver md;
 
@@ -23,13 +33,50 @@ public abstract class PlaylistGenerator {
 	public PlaylistGenerator(MongoDriver md) {
 		this.md = md;
 	}
-	
+
 	public abstract List<Document> generatePlaylist(final List<Document> currentPlaylist, List<Document> requests);
-	
-	protected Document createFromRequest(Document d, int rank) {
+
+	public static Document createFromRequest(Document d, int rank) {
 		return new Document("rank", rank).append("track_id", d.getObjectId("track_id"))
 				.append("req_id", d.getObjectId("_id")).append("user", d.getString("user"))
 				.append("timestamp", d.getInteger("timestamp"));
+	}
+
+	public static void writeDocumentsToStream(List<Document> docs, OutputStream os) {
+		PrintStream out = new PrintStream(os);
+
+		for (Document d : docs) {
+			out.println(DOC_HEADER);
+			out.println(d.toJson());
+		}
+	}
+
+	public static List<Document> readDocumentsFromStream(InputStream is) throws IOException {
+		List<Document> out = new ArrayList<Document>();
+		
+		StringBuilder sb = null;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		String tmp;
+		while((tmp = reader.readLine()) != null) {
+			if(tmp.startsWith(DOC_HEADER)) {
+				if(sb != null) {
+					out.add(Document.parse(sb.toString()));
+				}
+				
+				sb = new StringBuilder();
+			} else {
+				
+				if(sb == null)
+					throw new RuntimeException("Input did not start with a valid header!");
+				
+				sb.append(tmp);
+				
+			}
+		}
+		
+		
+		return out;
 	}
 
 }
